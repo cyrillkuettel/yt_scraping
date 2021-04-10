86f3b3ed786e23200befed736eca75a3696d991f
Cyrill <cyrillkuettel@gmail.com>
2021-04-10 18:16:56 +0200

    download thumbails to folder

--- download_thumbnails.py
+++ download_thumbnails.py
@@ -1,35 +1,55 @@
 #!/home/cyrill/anaconda3/envs/youtube_history_extractor/bin/python
-
 import json
 import requests
-json_file_name = "sample_data.json"
+import pathlib
+
+json_file_name = "sample_data_less.json" # in the future, this will be a command line argument args[]
 file1= open(json_file_name, 'r')
 Lines = file1.readlines()
-jsonList = [] # Initialize emtpy list
-count = -1
-for line in Lines:
-	count = count + 1
-	#print(line)
-	try:
-		data = json.loads(line) # sucessfully loaded a json objet. note there is also json.load without an 's'
-		jsonList.append(data)
-	except Exception as e:
-		print(e)
-		
-c = 0
-for dic_t in jsonList:
-	print(dic_t["thumbnail"])
-	url = dic_t["thumbnail"]
-	try:
-		r = requests.get(url, allow_redirects=True)
-		file_name_to_write = "{}{}".format("thumbnail",c)
-		open(file_name_to_write, 'wb').write(r.content)
-	except Exception as e:
-		print("{}{}".fomat("failed, here is the iteration", c))
-		raise e
-	c = c+1
 
-#idea. Json can also be avoided, and just use python dict I think.... This would simplify the code tremendously
+def loadEachVideoAsJsonIntoArray(Lines):
+	jsonList = [] 
+	for line in Lines:
+		print(line)
+		try:
+			data = json.loads(line) # sucessfully loaded a json objet. note there is also json.load without an 's'
+			jsonList.append(data)
+		except Exception as e:
+			print("{} {}".format("could not load json for this video. Probably url not valid. " ,e))
+	return jsonList
+
+def downloadThumbnails(jsonList):
+	folderName = "thumbnails"
+	pathlib.Path(folderName).mkdir(parents=True, exist_ok=True) #create the folder
+	c = 0
+	for dic_t in jsonList: # each object in array jsonList is basically a python dicitonary
+		videoId = getID(dic_t["url"])
+		completePath = folderName + "_" + videoId # just use os.path for this.
+		url = dic_t["thumbnail"]
+		try:
+			r = requests.get(url, allow_redirects=True) # instead of c, use the id of this particular video
+			open(completePath, 'wb').write(r.content)
+		except Exception as e:
+			print("{}{}".format("requests failed for thumbail. Iteration =", c))
+			raise e
+		c = c+1
+
+def getID(videoUrl):
+	trimBefore = videoUrl[0:32] # Is equal to "https://www.youtube.com/watch?v="
+	s = videoUrl.replace(trimBefore, "")
+	return s
+
+
+
+if __name__ == "__main__":
+	jsonList = loadEachVideoAsJsonIntoArray(Lines)
+	downloadThumbnails(jsonList)
+
+
+#craete folder in pyhton to store thumbnails. 
+# get the id and name the thumbnail after the id ( very good idea)
+
+#idea. Json is not really necessary , and just use python dict I think.... This would simplify the code tremendously
 #consider: Some objects may have not the expected values.
 # Test this! If it occurs, this incidence should not confound the correct chronological order. Just jump to the next one.
 
