db5723714cdfb440f9ba53b90f9e9f6407fccf9e
Cyrill <cyrillkuettel@gmail.com>
2021-04-18 16:29:22 +0200

    aufräumen

--- download_thumbnails.py
+++ download_thumbnails.py
@@ -1,56 +1,54 @@
-#!/home/cyrill/anaconda3/envs/youtube_history_extractor/bin/python
 import json
 import requests
 import pathlib
 
-json_file_name = "sample_data_less.json" # in the future, this will be a command line argument args[]
-file1= open(json_file_name, 'r')
+json_file_name = "sample_data.json"  # in the future, this will be a command line argument args[]
+file1 = open(json_file_name, 'r')
 Lines = file1.readlines()
 
+
 def loadEachVideoAsJsonIntoArray(Lines):
-	jsonList = [] 
-	for line in Lines:
-		print(line)
-		try:
-			data = json.loads(line) # sucessfully loaded a json objet. note there is also json.load without an 's'
-			jsonList.append(data)
-		except Exception as e:
-			print("{} {}".format("could not load json for this video. Probably url not valid. " ,e))
-	return jsonList
+    jsonList = []
+    for line in Lines:
+        print(line)
+        try:
+            data = json.loads(line)  # successfully loaded a json objet. note there is also json.load without an 's'
+            jsonList.append(data)
+        except Exception as e:
+            print("{} {}".format("could not load json for this video. Probably url not valid. ", e))
+    return jsonList
+
 
 def downloadThumbnails(jsonList):
-	folderName = "thumbnails"
-	pathlib.Path(folderName).mkdir(parents=True, exist_ok=True) #create the folder
-	c = 0
-	for dic_t in jsonList: # each object in array jsonList is basically a python dicitonary
-		videoId = getID(dic_t["url"])
-		completePath = folderName + "_" + videoId # just use os.path for this.
-		url = dic_t["thumbnail"]
-		try:
-			r = requests.get(url, allow_redirects=True) # instead of c, use the id of this particular video
-			open(completePath, 'wb').write(r.content)
-		except Exception as e:
-			print("{}{}".format("requests failed for thumbail. Iteration =", c))
-			raise e
-		c = c+1
+    folderName = "thumbnails"
+    pathlib.Path(folderName).mkdir(parents=True, exist_ok=True)  # create the folder
+    c = 0
+    for dic_t in jsonList:  # each object in array jsonList is basically a python dicitonary
+        videoId = getID(dic_t["url"])
+        completePath = folderName + "_" + videoId  # just use os.path for this.
+        url = dic_t["thumbnail"]
+        try:
+            r = requests.get(url, allow_redirects=True)  # instead of c, use the id of this particular video
+            open(completePath, 'wb').write(r.content)
+        except Exception as e:
+            print("{}{}".format("requests failed for thumbail. Iteration =", c))
+            raise e
+        c = c + 1
+
 
 def getID(videoUrl):
-	trimBefore = videoUrl[0:32] # Is equal to "https://www.youtube.com/watch?v="
-	s = videoUrl.replace(trimBefore, "")
-	return s
-
+    trimBefore = videoUrl[0:32]  # Is equal to "https://www.youtube.com/watch?v="
+    s = videoUrl.replace(trimBefore, "")
+    return s
 
 
 if __name__ == "__main__":
-	jsonList = loadEachVideoAsJsonIntoArray(Lines)
-	downloadThumbnails(jsonList)
+    jsonList = loadEachVideoAsJsonIntoArray(Lines)
+    downloadThumbnails(jsonList)
 
+# TODO:
+# it does not save thumbnails in the specified folder
 
-#craete folder in pyhton to store thumbnails. 
-# get the id and name the thumbnail after the id ( very good idea)
-
-#idea. Json is not really necessary , and just use python dict I think.... This would simplify the code tremendously
-#consider: Some objects may have not the expected values.
+# idea. Json is not really necessary , and just use python dict I think.... This would simplify the code tremendously
+# consider: Some objects may have not the expected values.
 # Test this! If it occurs, this incidence should not confound the correct chronological order. Just jump to the next one.
-
-
--- extracting_data.py
+++ extracting_data.py
@@ -1,5 +1,7 @@
 #!/home/cyrill/anaconda3/envs/youtube_history_extractor/bin/python
 
+# run this before running download_thumbnails
+
 import urllib3
 import json
 import re
@@ -13,94 +15,93 @@ link_file = 'link_file.txt'
 my_objects = []
 
 
+class Entry:  # Stores a single Video. information [title, url thumbnail] is added to attributes.
+    title = ""
+    url = ""
+    thumbnail = ""
 
-class Entry: # Stores a single Video. information [title, url thumbnail] is added to attributes.
-	title = ""
-	url = ""
-	thumbnail = ""
-	def __str__(self):
-		return "Title: {} Url:  {}".format(self.title, self.url)
+    def __str__(self):
+        return "Title: {} Url:  {}".format(self.title, self.url)
 
-	def extractYoutubeIdFromUrl(self):
-		failed = False
-		try:
-			url_data = urlib3.parse_url.urlib3.parse_url(self.url)
-		except Exception as e:
-			failed = True
-	
-		try:
-			query = urlib3.parse_url.parse_qs(url_data.query)
-		except Exception as e:
-			failed = True
-		
-		try:
-			yt_id = query["v"][0]
-		except Exception as e:
-			failed = True
-		if not failed:
-			return yt_id
-		else:
-			return "ID_extraction_Failed"
+    def extractYoutubeIdFromUrl(self):
+        failed = False
+        try:
+            url_data = urlib3.parse_url.urlib3.parse_url(self.url)
+        except Exception as e:
+            failed = True
 
-	def __init__(self, title, url):
+        try:
+            query = urlib3.parse_url.parse_qs(url_data.query)
+        except Exception as e:
+            failed = True
 
-		title = title.replace('\n','')
-		title =  re.sub(' +', ' ', title) # remove whitspace in the middle
-		self.title = ''.join(title).encode('utf-8').strip()
-		self.url = ''.join(url).encode('utf-8').strip()
-		
-		yt_id = self.extractYoutubeIdFromUrl()
+        try:
+            yt_id = query["v"][0]
+        except Exception as e:
+            failed = True
+        if not failed:
+            return yt_id
+        else:
+            return "ID_extraction_Failed"
 
-		if yt_id == "ID_extraction_Failed":
-			self.thumbnail = "ID_extraction_Failed"
-		else:
-			thumbnail_string = "http://img.youtube.com/vi/" + yt_id + "/maxresdefault.jpg"
-			self.thumbnail = ''.join(thumbnail_string).encode('utf-8').strip()
+    def __init__(self, title, url):
 
+        title = title.replace('\n', '')
+        title = re.sub(' +', ' ', title)  # remove whitespace in the middle
+        self.title = ''.join(title).encode('utf-8').strip()
+        self.url = ''.join(url).encode('utf-8').strip()
 
+        yt_id = self.extractYoutubeIdFromUrl()
 
-#create Entry object, and add all the objects to array my_objects.
-def createObjects(filename): # create the array Of "json objects"
-	data = soup = BeautifulSoup(open(filename), "html.parser")
-	print("{")
-	for div in soup.findAll('div', attrs={'class':'content-cell'}):#selects the div which is imporant
-			failed = False
-			try:
-				url = div.find('a')['href']
-				title = div.find('a').contents[0]
-				if "https://www.youtube.com" in title: # avoid delete Videos
-					failed = True
-			except Exception as e:
-				failed = True
-
-			if not failed:
-				entry = Entry(title, url)
-				#my_objects.append(entr)
-				#print(entry)
-				print(vars(entry)) #not working, why
-	print("}") #close the json object
+        if yt_id == "ID_extraction_Failed":
+            self.thumbnail = "ID_extraction_Failed"
+        else:
+            thumbnail_string = "http://img.youtube.com/vi/" + yt_id + "/maxresdefault.jpg"
+            self.thumbnail = ''.join(thumbnail_string).encode('utf-8').strip()
 
 
-#Various methods, not currently in use
+# create Entry object, and add all the objects to array my_objects.
+def createObjects(filename):  # create the array Of "json objects"
+    data = soup = BeautifulSoup(open(filename), "html.parser")
+    print("{")
+    for div in soup.findAll('div', attrs={'class': 'content-cell'}):  # selects the div which is imporant
+        failed = False
+        try:
+            url = div.find('a')['href']
+            title = div.find('a').contents[0]
+            if "https://www.youtube.com" in title:  # avoid delete Videos
+                failed = True
+        except Exception as e:
+            failed = True
+
+        if not failed:
+            entry = Entry(title, url)
+            # my_objects.append(entr)
+            # print(entry)
+            print(vars(entry))  # not working, why
+    print("}")  # close the json object
+
+
+# Various methods, not currently in use
 def extract_div(filename):
-	soup = BeautifulSoup(open(filename), "html.parser")
-	for link in soup.find_all('a'):
-		print(link.get('href'))
+    soup = BeautifulSoup(open(filename), "html.parser")
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+
 
 def getOnlyVideoLinks_notChannel(link_file):
+    file1 = open(link_file, 'r')
+    Lines = file1.readlines()
 
-	file1 = open(link_file, 'r')
-	Lines = file1.readlines()
-	 
-	count = 0
-	# Strips the newline character
-	for line in Lines:
-		count += 1
-		line = line.strip()
-		if "channel/" not in line: # 
-			#print("Line{}: {}".format(count, line.strip())) # Interesting .format() idea which might be useful later. 
-			print(line.strip())
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        count += 1
+        line = line.strip()
+        if "channel/" not in line:  #
+            # print("Line{}: {}".format(count, line.strip())) # Interesting .format() idea which might be useful later.
+            print(line.strip())
 
 
 if __name__ == "__main__":
-	createObjects(test_filename_div)
+    createObjects(test_filename_div)
